6224c5366b5e31e6e88727ba287a542f753123d1
Tom Craig <tomcraig3080@gmail.com>
2024-01-18 10:03:45 +0000

    Did stuff

--- Main.py
+++ Main.py
@@ -8,9 +8,11 @@ import pyrr
 
 #user defined stuff
 
-#folder (within Lidar/Datasets)
-folder = "Edinburgh"
-display_normals = False
+#folder (within Datasets)
+folder = "Hebrides Aiginis"
+
+#colour_map picks between no colour, "normal" map, "intensity"(if included in laz file)
+colour_map = ""
 
 #dev stuff
 display_logs = True
@@ -74,9 +76,10 @@ def read_data(folder):
     z = laslist[0].Z
 
     intensities = np.asarray(laslist[0].intensity,dtype="float64")
-    red = np.asarray(laslist[0].red,dtype="float64")
+    """
+    red = np.asarray(laslist[0].scan_angle_rank,dtype="float64")
     green = np.asarray(laslist[0].green,dtype="float64")
-    blue = np.asarray(laslist[0].blue,dtype="float64")
+    blue = np.asarray(laslist[0].blue,dtype="float64")"""
 
     for las in laslist[1:]:
         x = np.append(x,las.X + (1_000 * (las.header.x_offset - laslist[0].header.x_offset)))
@@ -84,12 +87,12 @@ def read_data(folder):
         z = np.append(z,las.Z + (1_000 * (las.header.z_offset - laslist[0].header.z_offset)))
 
         intensities = np.append(intensities,np.asarray(las.intensity,dtype="float64"))
-        red = np.append(red,np.asarray(las.intensity,dtype="float64"))
-        green = np.append(green,np.asarray(las.intensity,dtype="float64"))
-        blue = np.append(blue,np.asarray(las.intensity,dtype="float64"))
+        """red = np.append(red,np.asarray(las.scan_angle_rank,dtype="float64"))
+        green = np.append(green,np.asarray(las.green,dtype="float64"))
+        blue = np.append(blue,np.asarray(las.blue,dtype="float64"))"""
 
-    print(len(intensities))
-    return x,y,z,intensities,red,green,blue
+    #print(len(intensities),len(x),len(blue))
+    return x,y,z,intensities#,red,green,blue
 
 
 def apply_bounds(x,y,z,bounds):
@@ -101,21 +104,30 @@ def apply_bounds(x,y,z,bounds):
     sizex = max(x)-min(x)
     sizey = max(y)-min(y)
 
-    box_x = bs[1][0]*sizex
+    box_x = bs[0][1]*sizex
     box_y = bs[1][1]*sizey
     box_a = bs[0][0]*sizex
-    box_b = bs[0][1]*sizey
-
+    box_b = bs[1][0]*sizey
 
     centre = [box_a+((box_x-box_a)/2),box_b+((box_y-box_b)/2),0]
 
     q = pyrr.Quaternion.from_eulers(np.radians(r))
     rotation = pyrr.matrix33.create_from_quaternion(q)
 
+    print(box_x,box_a,box_y,box_b)
+    print(centre)
+
     box_x -= centre[0]
     box_y -= centre[1]
     box_a -= centre[0]
     box_b -= centre[1]
+    
+    print(box_x,box_a,box_y,box_b)
+
+    box_x = np.sign(box_x)*(int(abs(box_x))+[1 if int(abs(box_x)) != abs(box_x) else 0][0])
+    box_y = np.sign(box_y)*(int(abs(box_y))+[1 if int(abs(box_y)) != abs(box_y) else 0][0])
+    box_a = np.sign(box_a)*(int(abs(box_a))+[1 if int(abs(box_a)) != abs(box_a) else 0][0])
+    box_b = np.sign(box_b)*(int(abs(box_b))+[1 if int(abs(box_b)) != abs(box_b) else 0][0])
 
 
     tpl = [box_a,box_y,0]@rotation.T
@@ -150,17 +162,24 @@ def apply_bounds(x,y,z,bounds):
 
 
 def create_pointcloud(xyz):
+    print(len(xyz))
     pcd = o3d.geometry.PointCloud()
-    pcd.points = o3d.utility.Vector3dVector(xyz[:,:3])
+    pcd.points = o3d.utility.Vector3dVector(xyz)
 
+    return pcd
+
+
+def crop_pointcloud(pcd):
     vol = o3d.visualization.read_selection_polygon_volume("bound.json")
     selected_pcd = vol.crop_point_cloud(pcd)
+
+    print(selected_pcd)
 
     return selected_pcd
 
 
 def convert_to_normalmap(pcd):
-    pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=1000, max_nn=30))
+    pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=1000, max_nn=10))
     normals = np.asarray(pcd.normals)
     normals = ((normals - normals.min(axis=0)) / (normals.max(axis=0) - normals.min(axis=0)))
 
@@ -177,35 +196,62 @@ def convert_to_normalmap(pcd):
     pcd.colors = o3d.utility.Vector3dVector(cn)
 
 
+def add_colours(pcd,red,green,blue):
+    e = np.zeros(shape = [len(intensities)])
+
+    r = red/max(red)
+    g = green
+    b = blue
+
+    print("sum:",np.sum(red),np.sum(green),np.sum(blue))
+
+    print(r[-20:],g[-20:],b[-20:])
+
+    cn = np.dstack([r,g,b])[0]
+
+    print(cn.shape,xyz.shape)
+    pcd.colors = o3d.utility.Vector3dVector(cn.astype(np.float64))
+
+    print(pcd.has_colors())
+
+
+def add_intensity(pcd,intensities):
+    e = np.zeros(shape = [len(intensities)])
+
+    print(sum(intensities))
+    c1 = intensities/max(intensities)
+    c2 = intensities/max(intensities)
+    c3 = intensities/max(intensities)
+
+    print(np.sum(c1)/len(c1),np.sum(c2)/len(c2),np.sum(c3)/len(c3))
+
+    cn = np.dstack([c1,c2,c3])[0]
+
+
+    print(cn.shape,xyz.shape)
+    pcd.colors = o3d.utility.Vector3dVector(cn.astype(np.float64))
+
+    print(pcd.has_colors())
+
+
 #----------------------------------FUNCTION DEFINITION END------------------------------------#
 
 
 bounds = read_bounds(folder)
-x,y,z,intensities,red,green,blue = read_data(folder)
+x,y,z,intensities = read_data(folder)#,red,green,blue = read_data(folder)
 
 xyz = apply_bounds(x,y,z,bounds)
 
-
 pcd = create_pointcloud(xyz)
-if display_normals:
+
+if colour_map == "normal":
     convert_to_normalmap(pcd)
 
-intensities = intensities[:-35]
-red = red[:-35]
-green = green[:-35]
-blue = blue[:-35]
+elif colour_map == "intensity":
+    add_intensity(pcd,intensities)
 
-e = np.zeros(shape = [len(intensities)])
-
-r = red/max(red)
-g = green/max(green)
-b = blue/max(blue)
-
-cn = np.dstack([r,g,b])[0]
+pcd = crop_pointcloud(pcd)
 
 
-print(cn.shape,xyz.shape)
-pcd.colors = o3d.utility.Vector3dVector(cn.astype(np.float64))
-print(pcd.has_colors())
 # Display the selected point cloud
 o3d.visualization.draw_geometries([pcd])
---
+++ data_search.py
@@ -0,0 +1,57 @@
+import open3d as o3d
+import laspy
+import numpy as np
+from os import listdir
+from os import path
+import pyrr
+
+folders = listdir("Datasets")
+
+def read_data(folder):
+    print(folder)
+
+    paths = [f for f in listdir(f'Datasets/{folder}') if f.endswith(".laz")]
+    laslist = [laspy.read(f'Datasets/{folder}/{path}') for path in paths]
+
+    
+    print(list(laslist[0].point_format.dimension_names))
+    print()
+    """
+    x = laslist[0].X
+    y = laslist[0].Y
+    z = laslist[0].Z
+
+    intensities = []
+    red = []
+    green = []
+    blue = []
+
+    if "intensity" in laslist[0].point_format.dimension_names:
+        intensities = np.asarray(laslist[0].intensity,dtype="float64")
+    if "red" in laslist[0].point_format.dimension_names:
+        red = np.asarray(laslist[0].red,dtype="float64")
+        green = np.asarray(laslist[0].green,dtype="float64")
+        blue = np.asarray(laslist[0].blue,dtype="float64")
+
+    for las in laslist[1:]:
+        x = np.append(x,las.X + (1_000 * (las.header.x_offset - laslist[0].header.x_offset)))
+        y = np.append(y,las.Y + (1_000 * (las.header.y_offset - laslist[0].header.y_offset)))
+        z = np.append(z,las.Z + (1_000 * (las.header.z_offset - laslist[0].header.z_offset)))
+
+        if "intensity" in las.point_format.dimension_names:
+            intensities = np.append(intensities,np.asarray(las.intensity,dtype="float64"))
+        if "red" in las.point_format.dimension_names:
+            red = np.append(red,np.asarray(las.red,dtype="float64"))
+            green = np.append(green,np.asarray(las.green,dtype="float64"))
+            blue = np.append(blue,np.asarray(las.blue,dtype="float64"))
+
+
+    print("intensities:",np.sum(intensities))
+    print("red:",np.sum(red))
+    print("green:",np.sum(green))
+    print("blue:",np.sum(blue))
+
+    print()"""
+
+for i in folders:
+    read_data(i)
